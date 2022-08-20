ac3127d11f1770c14baf4745f2da9f8e4be2e3c9
bombr <47834716+bombr90@users.noreply.github.com>
2022-08-20 02:20:42 -0700

    File out of sync

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, 
         type: "asset/resource",
       },
     ],
